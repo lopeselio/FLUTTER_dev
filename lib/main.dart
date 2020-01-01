cb922a451c65951d14aa605b0a58e1f04f447bd5
lopeselio <lopeselio@gmail.com>
2020-01-02 00:57:04 +0530

    'first'

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s  your favourigte color?',
+      'What\'s your favourite animal?'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The Question!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 4'),
+              onPressed: answerQuestion,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
