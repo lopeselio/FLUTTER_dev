53707ecacc2dedd29d89a5b3fd8fc75e3f512417
lopeselio <lopeselio@gmail.com>
2020-01-03 23:58:50 +0530

    added margins and custom styling

--- lib/question.dart
+++ lib/question.dart
@@ -4,9 +4,16 @@ class Question extends StatelessWidget {
   final String questionText;
 
   Question(this.questionText);
-  
+
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+        child: Text(
+      questionText,
+      style: TextStyle(fontSize: 22),
+      textAlign: TextAlign.center,
+    ));
   }
 }
